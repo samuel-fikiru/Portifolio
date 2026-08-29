ad29b1e86d2edcaeed36d65a2c9ee1b0c05e062c
samuel <smfikiru@gmail.com>
2026-08-29 03:11:18 +0300

    working hamburger menu  and responsive nav bar

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const humbergurIcon = document.querySelector('.ham-burger');
+const humbergurBtn = document.querySelector('.ham-burger');
 const humbergerMenu = document.querySelector('.hamburger-menu');
+const crossBtn = document.querySelector('.cross-btn');
 
-humbergurIcon.addEventListener('click', ()=>{
+humbergurBtn.addEventListener('click', ()=>{
     humbergerMenu.classList.add('hamburger-active');
+    humbergurBtn.style.display='none';
+    crossBtn.style.display='block';
+})
+crossBtn.addEventListener('click', ()=>{
+    humbergerMenu.classList.remove('hamburger-active');
+    crossBtn.style.display='none';
+    humbergurBtn.style.display='block';
 })
