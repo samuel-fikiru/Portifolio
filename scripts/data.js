28c5bab106abbdf463cca6fe333f907769edf9cf
samuel <smfikiru@gmail.com>
2026-09-01 02:41:08 +0300

    created js produuc data

---
+++ scripts/data.js
@@ -0,0 +1,52 @@
+const projects = [
+    {
+        title:'Weather App',
+        description:'Weather forecast based on location.',
+        demoAddress:'images/project-demo/weather-app.png',
+        liveLink:'https://weather-app-one-beta-46.vercel.app/',
+        repolink:'https://github.com/samuel-fikiru/Weather-App',
+        type:'webApp'
+    },{
+        title:'Shopping List',
+        description:'Shopping list with add, remove, and update items.',
+        demoAddress:'images/project-demo/shoppingList.png',
+        liveLink:'https://shopping-list-phi-peach.vercel.app/',
+        repolink:'https://github.com/samuel-fikiru/Shopping-List',
+        type:'webApp'
+    },{
+        title:'Todo List',
+        description:'Task management with add, edit, delete features.',
+        demoAddress:'images/project-demo/todolistUI.png',
+        liveLink:'https://todo-list-nine-psi-23.vercel.app/',
+        repolink:'https://github.com/samuel-fikiru/TodoList',
+        type:'webApp'
+    },{
+        title:'Expense Tracker',
+        description:'Budget management with transactions and balance.',
+        demoAddress:'images/project-demo/expenseTracker.png',
+        liveLink:'https://expense-tracker-five-dun-96.vercel.app/',
+        repolink:'https://github.com/samuel-fikiru/Expense-Tracker',
+        type:'webApp'
+    },{
+        title:'Rock Paper Scissor',
+        description:'Classic game with interactive UI.',
+        demoAddress:'images/project-demo/rockPaperScissor.png',
+        liveLink:'https://rock-paper-scissors-gamma-amber.vercel.app',
+        repolink:'https://github.com/samuel-fikiru/Rock-Paper-Scissors',
+        type:'experiment'
+    },{
+        title:'Profile Cad',
+        description:'Personal social media profile card.',
+        demoAddress:'images/project-demo/profile-card.png',
+        liveLink:'https://expense-tracker-five-dun-96.vercel.app/',
+        repolink:'https://github.com/samuel-fikiru/Expense-Tracker',
+        type:'design'
+    },{
+        title:'Calculator',
+        description:'Functional simple calculator',
+        demoAddress:'images/project-demo/calculator.png',
+        liveLink:'https://calculator-pi-three-67.vercel.app/',
+        repolink:'https://github.com/samuel-fikiru/calculator',
+        type:'experiment'
+    },
+]
