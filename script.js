87580398dade7809d1354bd75015037392083579
samuel <smfikiru@gmail.com>
2026-09-01 02:55:36 +0300

    project catagory handler event

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {projects as projectData} from "./scripts/data.js"
+import { projects as projectData } from "./scripts/data.js";
 
 const humbergerMenu = document.querySelector(".hamburger-menu");
 const humbergurBtn = document.querySelector(".humbergur-btn");
@@ -7,22 +7,30 @@ const spans = document.querySelectorAll(".humbergur-btn span");
 let click = false;
 // humbergur menu handler
 humbergurBtn.addEventListener("click", () => {
-    humbergurBtn.classList.toggle("active");
-    console.log("working");
+  humbergurBtn.classList.toggle("active");
+  console.log("working");
 
-    if (!click) {
-        humbergerMenu.classList.add("hamburger-active");
-        spans.forEach((span) => {
-            span.style.backgroundColor = "white";
-        });
-        click = true;
-    } else {
-        humbergerMenu.classList.remove("hamburger-active");
-        spans.forEach((span) => {
-            span.style.backgroundColor = "black";
-        });
-        click = false;
-    }
+  if (!click) {
+    humbergerMenu.classList.add("hamburger-active");
+    spans.forEach((span) => {
+      span.style.backgroundColor = "white";
+    });
+    click = true;
+  } else {
+    humbergerMenu.classList.remove("hamburger-active");
+    spans.forEach((span) => {
+      span.style.backgroundColor = "black";
+    });
+    click = false;
+  }
 });
 
-console.log(projectData);
+const projectCatagoryBar = document.querySelector(".js-project-catagory-bar");
+const projectsContainer = document.querySelector(".selectd-works");
+// console.log(projectData);
+
+projectCatagoryBar.addEventListener("click", (e) => {
+  if (e.target.classList.contains("catagory")) {
+    console.log(e.target.textContent);
+  }
+});
--- scripts/data.js
+++ scripts/data.js
@@ -5,48 +5,48 @@ export const projects = [
         demoAddress:'images/project-demo/weather-app.png',
         liveLink:'https://weather-app-one-beta-46.vercel.app/',
         repolink:'https://github.com/samuel-fikiru/Weather-App',
-        type:'webApp'
+        type:'Web App'
     },{
         title:'Shopping List',
         description:'Shopping list with add, remove, and update items.',
         demoAddress:'images/project-demo/shoppingList.png',
         liveLink:'https://shopping-list-phi-peach.vercel.app/',
         repolink:'https://github.com/samuel-fikiru/Shopping-List',
-        type:'webApp'
+        type:'Web App'
     },{
         title:'Todo List',
         description:'Task management with add, edit, delete features.',
         demoAddress:'images/project-demo/todolistUI.png',
         liveLink:'https://todo-list-nine-psi-23.vercel.app/',
         repolink:'https://github.com/samuel-fikiru/TodoList',
-        type:'webApp'
+        type:'Web App'
     },{
         title:'Expense Tracker',
         description:'Budget management with transactions and balance.',
         demoAddress:'images/project-demo/expenseTracker.png',
         liveLink:'https://expense-tracker-five-dun-96.vercel.app/',
         repolink:'https://github.com/samuel-fikiru/Expense-Tracker',
-        type:'webApp'
+        type:'Web App'
     },{
         title:'Rock Paper Scissor',
         description:'Classic game with interactive UI.',
         demoAddress:'images/project-demo/rockPaperScissor.png',
         liveLink:'https://rock-paper-scissors-gamma-amber.vercel.app',
         repolink:'https://github.com/samuel-fikiru/Rock-Paper-Scissors',
-        type:'experiment'
+        type:'Experiments'
     },{
         title:'Profile Cad',
         description:'Personal social media profile card.',
         demoAddress:'images/project-demo/profile-card.png',
         liveLink:'https://expense-tracker-five-dun-96.vercel.app/',
         repolink:'https://github.com/samuel-fikiru/Expense-Tracker',
-        type:'design'
+        type:'Designs'
     },{
         title:'Calculator',
         description:'Functional simple calculator',
         demoAddress:'images/project-demo/calculator.png',
         liveLink:'https://calculator-pi-three-67.vercel.app/',
         repolink:'https://github.com/samuel-fikiru/calculator',
-        type:'experiment'
+        type:'Experiments'
     },
 ]
