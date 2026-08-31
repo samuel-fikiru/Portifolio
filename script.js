9d63a6cf41e6fd76ffb450b84b59fb2a17ebea79
samuel <smfikiru@gmail.com>
2026-09-01 01:09:07 +0300

    smooth modern hamburger

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const humbergurBtn = document.querySelector('.ham-burger');
-const humbergerMenu = document.querySelector('.hamburger-menu');
-const crossBtn = document.querySelector('.cross-btn');
+const humbergerMenu = document.querySelector(".hamburger-menu");
+const humbergurBtn = document.querySelector(".humbergur-btn");
+const spans = document.querySelectorAll(".humbergur-btn span");
 
-humbergurBtn.addEventListener('click', ()=>{
-    humbergerMenu.classList.add('hamburger-active');
-    humbergurBtn.style.display='none';
-    crossBtn.style.display='block';
-})
-crossBtn.addEventListener('click', ()=>{
-    humbergerMenu.classList.remove('hamburger-active');
-    crossBtn.style.display='none';
-    humbergurBtn.style.display='block';
-})
+let click = false;
+humbergurBtn.addEventListener("click", () => {
+  humbergurBtn.classList.toggle("active");
+  console.log("working");
+
+  if (!click) {
+    humbergerMenu.classList.add("hamburger-active");
+    spans.forEach((span) => {
+      span.style.backgroundColor = "white";
+    });
+    click = true;
+  } else {
+    humbergerMenu.classList.remove("hamburger-active");
+    spans.forEach((span) => {
+      span.style.backgroundColor = "black";
+    });
+    click = false;
+  }
+});
